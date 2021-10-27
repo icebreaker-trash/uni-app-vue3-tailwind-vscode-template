1c246f4d92ab891302a81aa420efad23f770efa8
SonOfMagic <qq1324318532@gmail.com>
2021-10-27 21:21:00 +0800

    fix: tailwind 样式类的单位 rpx 没有被 uniapp 转换

--- postcss.config.js
+++ postcss.config.js
@@ -17,8 +17,11 @@ module.exports = {
     require('autoprefixer')({
       remove: process.env.UNI_PLATFORM !== 'h5'
     }),
-    require('@dcloudio/vue-cli-plugin-uni/packages/postcss'),
+    // Thanks to lcysgsg [https://github.com/lcysgsg]
     require('tailwindcss')({ config: './tailwind.config.js' }),
+
+    require('@dcloudio/vue-cli-plugin-uni/packages/postcss'),
+    
     // [ WXSS 文件编译错误]
     //.focus-within\:sr-only:focus-within{
     // 不认识这个 \: 我们需要replace
